8c9a89e7e155562efe0178af83d4c9224945224c
kaue de matos <kaue178hotimail@gmail.com>
2024-01-18 19:46:43 -0300

    feature:adicionando client

--- src/test/java/com/email/email/EmailApplicationTests.java
+++ src/test/java/com/email/email/EmailApplicationTests.java
@@ -2,8 +2,10 @@ package com.email.email;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
+@EnableDiscoveryClient
 class EmailApplicationTests {
 
 	@Test
